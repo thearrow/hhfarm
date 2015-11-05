a58c4ebb68adae2cb13eff6b68fffe27f4446f58
Jake Klingler <thearrow3456@gmail.com>
2015-11-04 17:32:00 -0700

    add travis and prose configs

--- config.rb
+++ config.rb
@@ -28,10 +28,6 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
-
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
@@ -48,25 +44,19 @@ end
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   # activate :minify_css
-
-  # Minify Javascript on build
   # activate :minify_javascript
-
-  # Enable cache buster
   # activate :asset_hash
-
-  # Use relative URLs
   # activate :relative_assets
-
-  # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+end
+
